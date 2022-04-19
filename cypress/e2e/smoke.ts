04d47fee0526931f9f623e1cea8588cc047957d4
David Jebing <david.jebing@lise.de>
2022-04-19 16:55:27 +0200

    remove login test

--- cypress/e2e/smoke.ts
+++ cypress/e2e/smoke.ts
@@ -5,34 +5,12 @@ describe("smoke tests", () => {
     cy.cleanupUser();
   });
 
-  it("should allow you to register and login", () => {
-    const loginForm = {
-      email: `${faker.internet.userName()}@example.com`,
-      password: faker.internet.password(),
-    };
-    cy.then(() => ({ email: loginForm.email })).as("user");
-
-    cy.visit("/");
-    cy.findByRole("link", { name: /sign up/i }).click();
-
-    cy.findByRole("textbox", { name: /email/i }).type(loginForm.email);
-    cy.findByLabelText(/password/i).type(loginForm.password);
-    cy.findByRole("button", { name: /create account/i }).click();
-
-    cy.findByRole("link", { name: /notes/i }).click();
-    cy.findByRole("button", { name: /logout/i }).click();
-    cy.findByRole("link", { name: /log in/i });
-  });
-
   it("should allow you to make a note", () => {
     const testNote = {
       title: faker.lorem.words(1),
       body: faker.lorem.sentences(1),
     };
-    cy.login();
-    cy.visit("/");
-
-    cy.findByRole("link", { name: /notes/i }).click();
+    cy.visit("/notes");
     cy.findByText("No notes yet");
 
     cy.findByRole("link", { name: /\+ new note/i }).click();
