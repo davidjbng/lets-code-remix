a01cafdad4da9edec9435f00a4aeeff2f03fa658
David Jebing <david.jebing@lise.de>
2022-04-19 19:25:08 +0200

    fix e2e tests

--- cypress/e2e/notes.e2e.ts
+++ cypress/e2e/notes.e2e.ts
@@ -1,26 +1,18 @@
 import faker from "@faker-js/faker";
 
-describe("smoke tests", () => {
-  afterEach(() => {
-    cy.cleanupUser();
-  });
-
+describe("notes", () => {
   it("should allow you to make a note", () => {
-    const testNote = {
-      title: faker.lorem.words(1),
-      body: faker.lorem.sentences(1),
-    };
     cy.visit("/notes");
-    cy.findByText("No notes yet");
-
     cy.findByRole("link", { name: /\+ new note/i }).click();
 
+    const testNote = {
+      title: "test note",
+      body: faker.lorem.sentences(1),
+    };
     cy.findByRole("textbox", { name: /title/i }).type(testNote.title);
     cy.findByRole("textbox", { name: /body/i }).type(testNote.body);
     cy.findByRole("button", { name: /save/i }).click();
 
-    cy.findByRole("button", { name: /delete/i }).click();
-
-    cy.findByText("No notes yet");
+    cy.findByRole("link", { name: /test note/i });
   });
 });
