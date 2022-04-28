6a95ac453abe524a0a3dfde9c762227a038bcb44
David Jebing <david.jebing@lise.de>
2022-04-28 11:21:26 +0200

    move header nav to root

--- app/root.tsx
+++ app/root.tsx
@@ -12,6 +12,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { NavLink } from "react-router-dom";
 
 import tailwindStylesheetUrl from "./styles/tailwind.css";
 
@@ -39,11 +40,34 @@ export default function App() {
         <Links />
       </head>
       <body className="h-full">
-        <Outlet />
+        <div className="flex h-full min-h-screen flex-col">
+          <header className="flex items-center justify-between bg-slate-800 p-4 text-white">
+            <nav className="flex items-center justify-between gap-8">
+              <NavLinkItem to="notes">Notes</NavLinkItem>
+            </nav>
+          </header>
+          <Outlet />
+        </div>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
+  );
+}
+
+function NavLinkItem({
+  to,
+  children,
+}: React.PropsWithChildren<{ to: string }>) {
+  return (
+    <NavLink
+      to={to}
+      className={({ isActive }) =>
+        `text-3xl text-slate-300 ${isActive ? "font-bold text-slate-50" : ""}`
+      }
+    >
+      {({ isActive }) => (isActive ? <h1>{children}</h1> : children)}
+    </NavLink>
   );
 }
--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -18,45 +18,37 @@ export default function NotesPage() {
   const data = useLoaderData() as LoaderData;
 
   return (
-    <div className="flex h-full min-h-screen flex-col">
-      <header className="flex items-center justify-between bg-slate-800 p-4 text-white">
-        <h1 className="text-3xl font-bold">
-          <PendingLink to=".">Notes</PendingLink>
-        </h1>
-      </header>
+    <main className="flex h-full bg-white">
+      <div className="h-full w-80 border-r bg-gray-50">
+        <PendingLink to="new" className="block p-4 text-xl text-blue-500">
+          + New Note
+        </PendingLink>
 
-      <main className="flex h-full bg-white">
-        <div className="h-full w-80 border-r bg-gray-50">
-          <PendingLink to="new" className="block p-4 text-xl text-blue-500">
-            + New Note
-          </PendingLink>
+        <hr />
 
-          <hr />
+        {data.noteListItems.length === 0 ? (
+          <p className="p-4">No notes yet</p>
+        ) : (
+          <ol>
+            {data.noteListItems.map((note) => (
+              <li key={note.id}>
+                <PendingNavLink
+                  className={({ isActive }) =>
+                    `block border-b p-4 text-xl ${isActive ? "bg-white" : ""}`
+                  }
+                  to={note.id}
+                >
+                  📝 {note.title}
+                </PendingNavLink>
+              </li>
+            ))}
+          </ol>
+        )}
+      </div>
 
-          {data.noteListItems.length === 0 ? (
-            <p className="p-4">No notes yet</p>
-          ) : (
-            <ol>
-              {data.noteListItems.map((note) => (
-                <li key={note.id}>
-                  <PendingNavLink
-                    className={({ isActive }) =>
-                      `block border-b p-4 text-xl ${isActive ? "bg-white" : ""}`
-                    }
-                    to={note.id}
-                  >
-                    📝 {note.title}
-                  </PendingNavLink>
-                </li>
-              ))}
-            </ol>
-          )}
-        </div>
-
-        <div className="flex-1 p-6">
-          <Outlet />
-        </div>
-      </main>
-    </div>
+      <div className="flex-1 p-6">
+        <Outlet />
+      </div>
+    </main>
   );
 }
